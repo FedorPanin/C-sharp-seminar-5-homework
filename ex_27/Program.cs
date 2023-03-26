e74f133f4e7e4f9c2fa0cc6506078caa49077883
Mess <alla-fedia@mail.ru>
2023-03-26 12:39:01 +0300

    first commit

---
+++ ex_27/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход целое или дробное число и выдаёт количество цифр в числе.
+
+
+Console.WriteLine("введите число");
+double r = Convert.ToDouble(Console.ReadLine());
+string s = Convert.ToString(r);
+int sum = 0;
+
+for (int i = 0; i < s.Length; i++)
+{
+    if (char.IsNumber(s[i]))
+    {
+
+        sum += Convert.ToInt32(s[i].ToString());
+    }
+}
+Console.WriteLine("сумма всех цифр в числе равна: " + sum);
---
+++ ex_34/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+int size = 10;
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+    if (numbers[z] % 2 == 0)
+        count++;
+        
+Console.WriteLine($"{count} из {numbers.Length} чётные");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100, 1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
